3d09d591a4a0ddecd102071b264283cc65f21304
H-Achref <zacachref123@gmail.com>
2021-03-14 22:07:21 +0100

    add adminControler

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/Admin", name="admin")
+     */
+    public function index(): Response
+    {
+        $agences =[
+            1=>
+            array(
+            
+                "id" => "1",
+                "nom"=>"agence1",
+                "telAgence"=>"24335154",
+                "adresseVille"=>"tunis",
+                "actions"=>"action1",
+
+            ),
+            2=>array(
+            
+                "id" => "2",
+                "nom"=>"agence2",
+                "telAgence"=>"2555445",
+                "adresseVille"=>"Berline",
+                "actions"=>"action2",
+
+            ),
+            8=>array(
+            
+                "id" => "3",
+                "nom"=>"agnece3",
+                "telAgence"=>"2455645",
+                "adresseVille"=>"Paris",
+                "actions"=>"action3",
+
+            ),
+
+        ];
+      /*  $agence= array(
+            
+            "id" => "4",
+            "nom"=>"agnece3",
+            "telAgence"=>"2455645",
+            "adresseVille"=>"Paris",
+            "actions"=>"action3",
+
+        );
+        $agences[1]=$agence;
+        array_push($agences, $agence);
+        dd($agences);   */
+        $voitures =[
+            array(
+        
+                "id"=>"1",
+                "marque"=>"Toyota",
+                "couleur"=>"noir",
+                "description"=>"tres puissante",
+                "nommbreDePlaces"=>"5",
+                "nomAgence"=>"agence1",
+                "actions"=>"action1",
+
+            ),
+            array(
+        
+                "id"=>"2",
+                "marque"=>"Alfa Romio",
+                "couleur"=>"bleu",
+                "description"=>"tres pratique",
+                "nommbreDePlaces"=>"2",
+                "nomAgence"=>"agence2",
+                "actions"=>"action2",
+
+            ),
+
+        ];
+        //dd($agences);
+
+        return $this->render('admin/index.html.twig', [
+            "agences"=>$agences,
+            "voitures"=>$voitures,
+
+        ]);
+    }
+}
