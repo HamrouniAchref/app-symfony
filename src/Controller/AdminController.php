06036987c5ddfa3745dffa1021bc3ebbaeb8070f
H-Achref <zacachref123@gmail.com>
2021-04-25 06:28:32 +0200

    update admin controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Agence;
+use App\Entity\Voiture;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
@@ -13,76 +15,12 @@ class AdminController extends AbstractController
      */
     public function index(): Response
     {
-        $agences =[
-            1=>
-            array(
-            
-                "id" => "1",
-                "nom"=>"agence1",
-                "telAgence"=>"24335154",
-                "adresseVille"=>"tunis",
-         
+        $entityManager = $this->getDoctrine()->getManager();
+        $agences = $entityManager->getRepository(Agence::class)->findAll();
+        $voitures = $entityManager->getRepository(Voiture::class)->findAll();
 
-            ),
-            2=>array(
-            
-                "id" => "2",
-                "nom"=>"agence2",
-                "telAgence"=>"2555445",
-                "adresseVille"=>"Berline",
-                
-
-            ),
-            8=>array(
-            
-                "id" => "3",
-                "nom"=>"agnece3",
-                "telAgence"=>"2455645",
-                "adresseVille"=>"Paris",
-             
-
-            ),
-
-        ];
-      /*  $agence= array(
-            
-            "id" => "4",
-            "nom"=>"agnece3",
-            "telAgence"=>"2455645",
-            "adresseVille"=>"Paris",
-            "actions"=>"action3",
-
-        );
-        $agences[1]=$agence;
-        array_push($agences, $agence);
-        dd($agences);   */
-        $voitures =[
-            array(
-        
-                "id"=>"1",
-                "marque"=>"Toyota",
-                "couleur"=>"noir",
-                "description"=>"tres puissante",
-                "nommbreDePlaces"=>"5",
-                "nomAgence"=>"agence1",
-                
-
-            ),
-            array(
-        
-                "id"=>"2",
-                "marque"=>"Alfa Romio",
-                "couleur"=>"bleu",
-                "description"=>"tres pratique",
-                "nommbreDePlaces"=>"2",
-                "nomAgence"=>"agence2",
-             
-
-            ),
-
-        ];
-        //dd($agences);
-
+        //dd($voitures);
+    
         return $this->render('admin/index.html.twig', [
             "agences"=>$agences,
             "voitures"=>$voitures,
